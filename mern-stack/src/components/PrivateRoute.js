fe9b54f12599ec361182cfe152f7ea77b5261e9b
Douglas Belisario <douglasbelisariou@gmail.com>
2024-07-31 18:01:42 -0400

    first commit

--- mern-stack/src/components/PrivateRoute.js
+++ mern-stack/src/components/PrivateRoute.js
@@ -1,54 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
-function ListaUsuarios() {
-  const [usuarios, setUsuarios] = useState([]);
-
-  useEffect(() => {
-    const fetchUsuarios = async () => {
-      try {
-        const response = await axios.get("/api/user/users");
-        setUsuarios(response.data);
-      } catch (error) {
-        console.error("Error al obtener usuarios:", error);
-      }
-    };
-
-    fetchUsuarios();
-  }, []);
-
-  return (
-    <div>
-      <h2>Lista de usuarios</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th>Teléfono</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {usuarios.map((usuario) => (
-            <tr key={usuario._id}>
-              <td>{usuario.name}</td>
-              <td>{usuario.email}</td>
-              <td>{usuario.telefono}</td>
-              <td>
-                <button className="btn btn-primary mr-2">
-                  {/* Agregar funcionalidad para editar */}
-                </button>
-                <button className="btn btn-danger">
-                  {/* Agregar funcionalidad para eliminar */}
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+function PrivateRoute() {
+  const token = localStorage.getItem("token");
+  return token ? <Outlet /> : <Navigate to="/login" />;
 }
 
-export default ListaUsuarios;
+export default PrivateRoute;
